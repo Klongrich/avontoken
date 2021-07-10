afd5b9ede81e7359d0099a4321353d8772684097
klongrich <longrichk@gmail.com>
2021-07-09 22:59:19 -0400

    updating new header

--- src/Components/homepage.js
+++ src/Components/homepage.js
@@ -1,16 +1,35 @@
 import React from "react";
+import styled from "styled-components";
 
 import TokenBackground from "../assests/AvonTokenBackground.png";
 import SecondToken from "../assests/avonSecond.png";
 import TokenLogo from "../assests/AvonTokenLogo.png";
 
+const Container = styled.div`
+    background-color: black;
+
+    font-size: 64px;
+    font-family: 'Playball', cursive;
+    color: #FFFFFF;
+
+    p{
+        margin-left: 130px;
+        margin-top: -135px;
+    }
+`   
+
 export default function Homepage() {
 
     return (
         <>
+
+            <Container>
             <div Style="padding: 15px;">
                 <img src={TokenLogo} height="75" width="75" alt="" />
             </div>
+            
+                <p> Avon Token </p>
+            </Container>
 
             <img src={SecondToken} alt="" />
             <img src={TokenBackground} alt="" />
