2d27f0d105ac360c44fc97e85d9c463771db266d
klongrich <longrichk@gmail.com>
2021-07-09 22:21:45 -0400

    adding link to mainnet on etherscan

--- src/Components/homepage.js
+++ src/Components/homepage.js
@@ -10,9 +10,14 @@ export default function Homepage() {
             <div Style="padding: 15px;">
                 <img src={TokenLogo} height="75" width="75" alt="" />
             </div>
+
             <img src={TokenBackground} alt="" />
-            
+
+
+            <p Style="text-align:center;">Token Lanuched Here on Mainnet -> <a href="https://etherscan.io/token/0x7e992d8f57223661106c29e519e22a2a9a7bcefb"> https://etherscan.io/token/0x7e992d8f57223661106c29e519e22a2a9a7bcefb </a> </p>
+
             <div Style="text-align:center">
+                
                 <p>NEXT BIG THING</p>
                 <p>NOT A SCAM</p>
             </div>
