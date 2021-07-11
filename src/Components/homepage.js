446fbdc242714569caa4b7ec46f9cf812934919b
klongrich <longrichk@gmail.com>
2021-07-11 18:06:15 -0400

    updating to 3 link locations

--- src/Components/homepage.js
+++ src/Components/homepage.js
@@ -31,17 +31,11 @@ export default function Homepage() {
                 <p> Avon Token </p>
             </Container>
 
-            <img src={SecondToken} alt="" />
-            <img src={TokenBackground} alt="" />
-
-
             <p Style="text-align:center;">Token Lanuched Here on Mainnet -> <a href="https://etherscan.io/token/0x7e992d8f57223661106c29e519e22a2a9a7bcefb"> https://etherscan.io/token/0x7e992d8f57223661106c29e519e22a2a9a7bcefb </a> </p>
+            <p Style="text-align:center;">Purchase on Uniswap -> <a href="https://app.uniswap.org/#/swap?inputCurrency=0x7e992d8f57223661106c29e519e22a2a9a7bcefb"> https://app.uniswap.org/#/swap?inputCurrency=0x7e992d8f57223661106c29e519e22a2a9a7bcefb</a> </p>
+            <p Style="text-align:center;">See Listed on Uniswap -> <a href="https://info.uniswap.org/#/tokens/0x7e992d8f57223661106c29e519e22a2a9a7bcefb"> https://info.uniswap.org/#/tokens/0x7e992d8f57223661106c29e519e22a2a9a7bcefb</a> </p>
 
-            <div Style="text-align:center">
-                
-                <p>NEXT BIG THING</p>
-                <p>NOT A SCAM</p>
-            </div>
+
         </>
     )
 }
