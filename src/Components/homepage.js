e46bad11aae48fba602c7a6fb3375df492fc06c4
klongrich <longrichk@gmail.com>
2021-07-11 18:29:37 -0400

    adding basic info

--- src/Components/homepage.js
+++ src/Components/homepage.js
@@ -16,6 +16,47 @@ const Container = styled.div`
     }
 `   
 
+const BodyContainer = styled.div`
+    background-color: black;
+    text-align: center;
+    margin-top: -70px;
+    border-top: 1px solid white;
+    border-bottom: 1px solid white;
+
+    h2 {
+        color: white;
+        padding: 20px;
+    }
+
+    p {
+        color: white;
+        padding: 20px;
+    }
+
+    ul {
+        color: white;
+        list-style-type: none;
+        margin-left: -42px;
+    }
+
+    li {
+        padding-top: 10px;
+        padding-bottom: 10px;
+    }
+
+`
+
+const FooterContainer = styled.div`
+    background-color: black;
+    color: white;
+    padding-top: 25px;
+    padding-bottom: 25px;
+
+    a {
+        color: #0000EE;
+    }
+`
+
 export default function Homepage() {
 
     return (
@@ -29,11 +70,31 @@ export default function Homepage() {
                 <p> Avon Token </p>
             </Container>
 
-            <p Style="text-align:center;">Token Lanuched Here on Mainnet -> <a href="https://etherscan.io/token/0x7e992d8f57223661106c29e519e22a2a9a7bcefb"> https://etherscan.io/token/0x7e992d8f57223661106c29e519e22a2a9a7bcefb </a> </p>
-            <p Style="text-align:center;">Purchase on Uniswap -> <a href="https://app.uniswap.org/#/swap?inputCurrency=0x7e992d8f57223661106c29e519e22a2a9a7bcefb"> https://app.uniswap.org/#/swap?inputCurrency=0x7e992d8f57223661106c29e519e22a2a9a7bcefb</a> </p>
-            <p Style="text-align:center;">See Listed on Uniswap -> <a href="https://info.uniswap.org/#/tokens/0x7e992d8f57223661106c29e519e22a2a9a7bcefb"> https://info.uniswap.org/#/tokens/0x7e992d8f57223661106c29e519e22a2a9a7bcefb</a> </p>
+            <BodyContainer>
+                <h2> Avon Token is a next generation crypto-currency </h2>
 
+                <p>
+                   The recent lanuch of our ERC20 token will be used in future projects under the <strong> <u> AvonDAO </u> </strong>
+                </p>
 
+                <p>
+                    <strong>
+                        Future projects include:
+                    </strong>
+                </p>
+
+                <ul>
+                    <li>AvonNFT Market Place</li>
+                    <li>AvonDeFi Protocol</li>
+                    <li>AvonInTheKnow Page</li>
+                </ul>
+            </BodyContainer>
+
+            <FooterContainer>
+                <p Style="text-align:center;">Token Lanuched Here on Mainnet -> <a href="https://etherscan.io/token/0x7e992d8f57223661106c29e519e22a2a9a7bcefb"> https://etherscan.io/token/0x7e992d8f57223661106c29e519e22a2a9a7bcefb </a> </p>
+                <p Style="text-align:center;">Purchase on Uniswap -> <a href="https://app.uniswap.org/#/swap?inputCurrency=0x7e992d8f57223661106c29e519e22a2a9a7bcefb"> https://app.uniswap.org/#/swap?inputCurrency=0x7e992d8f57223661106c29e519e22a2a9a7bcefb</a> </p>
+                <p Style="text-align:center;">See Listed on Uniswap -> <a href="https://info.uniswap.org/#/tokens/0x7e992d8f57223661106c29e519e22a2a9a7bcefb"> https://info.uniswap.org/#/tokens/0x7e992d8f57223661106c29e519e22a2a9a7bcefb</a> </p>
+            </FooterContainer>
         </>
     )
 }
