16f2cbd58545dff2103752b1ec3691ccce46ec6d
klongrich <longrichk@gmail.com>
2021-07-13 00:00:35 -0400

    AvonDAO temp page update

--- src/Components/homepage.js
+++ src/Components/homepage.js
@@ -121,6 +121,7 @@ export default function Homepage() {
             <FooterContainer>
                 <p Style="text-align:center;">Token Lanuched Here on Mainnet -> <a href="https://etherscan.io/token/0x7e992d8f57223661106c29e519e22a2a9a7bcefb"> https://etherscan.io/token/0x7e992d8f57223661106c29e519e22a2a9a7bcefb </a> </p>
                 <p Style="text-align:center;">Purchase on Uniswap -> <a href="https://app.uniswap.org/#/swap?inputCurrency=0x7e992d8f57223661106c29e519e22a2a9a7bcefb"> https://app.uniswap.org/#/swap?inputCurrency=0x7e992d8f57223661106c29e519e22a2a9a7bcefb</a> </p>
+                
                 {/* <p Style="text-align:center;">See Listed on Uniswap -> <a href="https://info.uniswap.org/#/tokens/0x7e992d8f57223661106c29e519e22a2a9a7bcefb"> https://info.uniswap.org/#/tokens/0x7e992d8f57223661106c29e519e22a2a9a7bcefb</a> </p> */}
             
                 <SocialIconsContainer>
--- src/pages/avondao.js
+++ src/pages/avondao.js
@@ -7,6 +7,10 @@ const Container = styled.div`
 
     padding: 25px;
     height: 1200px;
+
+    a {
+        color:#0000EE;
+    }
 `
 
 export default function AvonDao () {
@@ -18,12 +22,31 @@ export default function AvonDao () {
                 <div Style="border-bottom: 1px solid white;">
                     <h2> Avon DAO  </h2>
                 </div>
-                <p>
-                    Under Construction .....
+                <p Style="padding-top: 15px;">
+                    All DeFi Projects In Once Place
                 </p>
 
                 <p>
-                    Coming soon .....
+                    Sick of seeing random tokens blow up for no reason? 
+                </p>
+
+                <p>
+                    Never seem to get in on them? 
+                </p>
+
+                <p> 
+                    Well congrats, you have found AvonToken! This page won't be up for much longer 
+                    however I invite you to join us with our token $AT.
+                </p>
+
+                <p Style="text-align: center; padding-top: 25px; padding-bottom: 25px;">
+                    Purchase Here: -> <a href="https://app.uniswap.org/#/swap?inputCurrency=0x7e992d8f57223661106c29e519e22a2a9a7bcefb"> https://app.uniswap.org/#/swap?inputCurrency=0x7e992d8f57223661106c29e519e22a2a9a7bcefb</a>
+                </p>
+
+                <p>
+                    We hope to see all of you on the moon. Remeber this is not financial advice.
+                    All investing comes with risk, with that said that doesn't mean this can't or
+                    won't go up 100x (: 
                 </p>
             </Container>
         </>
