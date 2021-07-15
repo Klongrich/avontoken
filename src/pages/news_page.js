d3d49cd63455ed040ac40b5a539a464a8129b18c
klongrich <longrichk@gmail.com>
2021-07-14 22:40:48 -0400

    removing unused vars

--- src/pages/news_page.js
+++ src/pages/news_page.js
@@ -1,6 +1,5 @@
 import React, {useState, useEffect} from "react";
 import styled from "styled-components";
-import Popup from "reactjs-popup";
 
 import TokenLogo from "../assests/AvonTokenLogo.png";
 import RocketVideo from "../assests/rocket.gif";
