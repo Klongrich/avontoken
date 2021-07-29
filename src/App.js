a04246412b620235649c6ea0c055c88e865d0565
klongrich <longrichk@gmail.com>
2021-07-29 02:20:56 -0400

    adding back normal screen

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-// import HomePage from "./pages/homepage";
-//import AvonDao from "./pages/avondao";
-// import NewsPage from "./pages/news_page";
-// import LogIn from "./pages/DeFi/login";
-// import VotePage from './pages/Goverence/vote'; 
+import HomePage from "./pages/homepage";
+import NewsPage from "./pages/news_page";
+import LogIn from "./pages/DeFi/login";
+import VotePage from './pages/Goverence/vote'; 
 
-// import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import FakeTakeDown from "./pages/fakeTakeDown";
+// import FakeTakeDown from "./pages/fakeTakeDown";
 
 function App() {
   return (
     <>
-      {/* <BrowserRouter>
+      <BrowserRouter>
         <Switch>
           <Route path="/" exact component={HomePage} />
           <Route path="/app" exact component={LogIn} />
           <Route path="/news" excat component={NewsPage} />
           <Route path="/vote" exact component={VotePage} />
         </Switch>
-      </BrowserRouter> */}
-
-      <FakeTakeDown />
+      </BrowserRouter>
       
     </>
   );
